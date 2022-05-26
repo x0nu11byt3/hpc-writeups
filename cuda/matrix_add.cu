83bef42a79e2601ce9dec99db5831440aa9403c9
x0nu11byt3 <x0nu11byt3@proton.me>
2022-05-26 17:32:30 -0500

    alert! segment violation (core generated) in matrix_add.cu

--- cuda/matrix_add.cu
+++ cuda/matrix_add.cu
@@ -7,7 +7,7 @@
 #define THREADS_PER_BLOCK 2
 #define N_BLOCKS 5
 
-/* Kernel definition vector add */
+/* Kernel definition matrix add */
 __global__ void matrix_add(float a[N_THREADS][N_THREADS], float b[N_THREADS][N_THREADS], float c[N_THREADS][N_THREADS]){
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
@@ -31,36 +31,35 @@ void fill_matrix_random(float **matrix, int min, int max){
 int main(int argc, char** argv) {
 
     /* Init host code */
-    size_t size = N_THREADS * sizeof(float);
+    size_t size = N_THREADS * N_THREADS * sizeof(float);
 
-    /* Allocate input vectors host_a & host_b in host memory (RAM) */
+    /* Allocate input matrix host_a & host_b in host memory (RAM) */
 
-    float *host_a = (float*)malloc(N_THREADS * size);
-    float *host_b = (float*)malloc(N_THREADS * size);
-    float *host_c = (float*)malloc(N_THREADS * size);
+    float *host_a = (float*)malloc(size);
+    float *host_b = (float*)malloc(size);
+    float *host_c = (float*)malloc(size);
 
-    /* Inicialize input vectors */
+    /* Inicialize input matrix */
     fill_matrix_random(&host_a, 1, 99);
     fill_matrix_random(&host_b, 1, 99);
 
-    /* Allocate vectors in device memory (VRAM) */
+    /* Allocate matrix in device memory (VRAM) */
     float *device_a, *device_b, *device_c;
-    cudaMalloc((void**)&device_a, N_THREADS * size);
-    cudaMalloc((void**)&device_b, N_THREADS * size);
-    cudaMalloc((void**)&device_c, N_THREADS * size);
+    cudaMalloc((void**)&device_a, size);
+    cudaMalloc((void**)&device_b, size);
+    cudaMalloc((void**)&device_c, size);
 
-    /* Copy input vectors from host memory to device memory */
-    cudaMemcpy(device_a, host_a, N_THREADS * size, cudaMemcpyHostToDevice);
-    cudaMemcpy(device_b, host_b, N_THREADS * size, cudaMemcpyHostToDevice);
+    /* Copy input matrix from host memory to device memory */
+    cudaMemcpy(device_a, host_a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_b, host_b, size, cudaMemcpyHostToDevice);
 
     /*Invoke Kernel */
     dim3 threadsPerBlock(N_THREADS,N_THREADS);
     dim3 numBlocks( N_THREADS / threadsPerBlock.x, N_THREADS / threadsPerBlock.y  );
     matrix_add<<<numBlocks,threadsPerBlock>>>(device_a,device_b,device_c);
-    //vector_add<<<1,N_THREADS>>>(device_a,device_b,device_c);
 
     /* Copy result from device memory to host memory */
-    cudaMemcpy(host_c,device_c, N_THREADS * size,cudaMemcpyDeviceToHost);
+    cudaMemcpy(host_c,device_c, size,cudaMemcpyDeviceToHost);
 
     for (int i = 0; i < N_THREADS; i++  ){
         for (int j = 0; j < N_THREADS; j++  )
